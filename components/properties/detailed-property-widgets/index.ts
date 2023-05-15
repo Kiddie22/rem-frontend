99f9466e5e4918fbc840f206ef0ac189b244d055
Rasula <rasula@surge.global>
2023-05-15 15:25:52 +0530

    refactor: export all widgets via index.ts

---
+++ components/properties/detailed-property-widgets/index.ts
@@ -0,0 +1,13 @@
+import PropertyDetailsCommands from './property-details-commands';
+import PropertyDetailsHeader from './property-details-header';
+import PropertyDetailsTasks from './property-details-tasks';
+import PropertyTenantDetails from './property-tenant-details';
+
+const widgets = {
+  PropertyDetailsCommands,
+  PropertyDetailsHeader,
+  PropertyDetailsTasks,
+  PropertyTenantDetails,
+};
+
+export default widgets;
--- pages/properties/[id].tsx
+++ pages/properties/[id].tsx
@@ -4,10 +4,7 @@ import { useRouter } from 'next/router';
 import { Property } from '@/utils/properties-utils';
 import useAxiosInstance from '@/hooks/useAxiosInstance';
 import queryKeys from '@/react-query/contants';
-import PropertyDetailsHeader from '@/components/properties/detailed-property-widgets/property-details-header';
-import PropertyTenantDetails from '@/components/properties/detailed-property-widgets/property-tenant-details';
-import PropertyDetailsCommands from '@/components/properties/detailed-property-widgets/property-details-commands';
-import PropertyDetailsTasks from '@/components/properties/detailed-property-widgets/property-details-tasks';
+import Widgets from '@/components/properties/detailed-property-widgets';
 
 type PropTypes = { property: Property };
 
@@ -30,10 +27,10 @@ function PropertyDetailsPage(): JSX.Element {
   return (
     <Container>
       <Grid container spacing={3} pt={3}>
-        <PropertyDetailsHeader property={property} />
-        <PropertyDetailsCommands property={property} />
-        <PropertyTenantDetails property={property} />
-        <PropertyDetailsTasks property={property} />
+        <Widgets.PropertyDetailsHeader property={property} />
+        <Widgets.PropertyDetailsCommands property={property} />
+        <Widgets.PropertyTenantDetails property={property} />
+        <Widgets.PropertyDetailsTasks property={property} />
       </Grid>
     </Container>
   );
